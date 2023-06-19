391e1d0b016c25fd5d6539aa8ed0bc5e5265eaf3
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-06-19 16:00:05 +0530

    Commented the code

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://lakshanrukantha.github.io");
+  const browser = await puppeteer.launch({ headless: false }); // Launch the browser
+  const page = await browser.newPage(); // Open a new tab in the browser
+  await page.goto("https://github.com/lakshanrukantha"); // Go to a new page
+
+  // Scrape functionality
   const scrapeData = await page.evaluate(() => {
-    const name = document.querySelector("#intro .container p");
+    const name = document.querySelector(".p-name.vcard-fullname"); // Select the full name
     return name.innerText;
   });
-  console.log(scrapeData);
-  await browser.close();
+
+  await page.screenshot({ path: "website.png" }); // Take a screenshot of the website
+
+  console.log(scrapeData); // Dsiplay scrape data
+
+  await browser.close(); // Close the browser
 })();
